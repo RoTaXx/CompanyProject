5c93341f252cefe100eb27706b9c78ae6cbe7307
RoTaXx <subofrotax@gmail.com>
2023-12-26 10:31:47 +0200

    Criteria Query Methods

--- src/main/java/org/example/dao/CompanyDao.java
+++ src/main/java/org/example/dao/CompanyDao.java
@@ -1,10 +1,16 @@
 package org.example.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.example.configuration.SessionFactoryUtil;
 import org.example.entity.Company;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class CompanyDao {
@@ -42,6 +48,124 @@ public class CompanyDao {
             Transaction transaction = session.beginTransaction();
             session.saveOrUpdate(company);
             transaction.commit();
+        }
+    }
+
+    public static void deleteCompany(Company company){
+        try(Session session = SessionFactoryUtil.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+            session.delete(company);
+            transaction.commit();
+        }
+    }
+
+    public static List<Company> companiesFindByInitialCapitalBetween(double bottom, double top) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Company> cr = cb.createQuery(Company.class);
+            Root<Company> root = cr.from(Company.class);
+
+            cr.select(root).where(cb.between(root.get("initialCapital"), bottom, top));
+
+            Query<Company> query = session.createQuery(cr);
+            List<Company> companies = query.getResultList();
+            return companies;
+        }
+    }
+
+    public static List<Company> findByNameStartingWithAndInitialCapitalGreaterThan(String name, double initialCapital) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Company> cr = cb.createQuery(Company.class);
+            Root<Company> root = cr.from(Company.class);
+
+            Predicate greaterThanInitialCapital = cb.greaterThan(root.get("initialCapital"), initialCapital);
+            Predicate nameStartingWith = cb.like(root.get("name"), name + "%");
+
+            cr.select(root).where(cb.and(nameStartingWith, greaterThanInitialCapital));
+
+            Query<Company> query = session.createQuery(cr);
+            List<Company> companies = query.getResultList();
+            return companies;
+        }
+    }
+
+    public static double sumInitialCapital() {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Double> cr = cb.createQuery(Double.class);
+            Root<Company> root = cr.from(Company.class);
+
+            cr.select(cb.sum(root.get("initialCapital")));
+
+            Query<Double> query = session.createQuery(cr);
+            double maxInitialCapital = query.getSingleResult();
+            return maxInitialCapital;
+        }
+    }
+
+    public static List<Company> companiesWithNameEqualTo(String name) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Company> cr = cb.createQuery(Company.class);
+            Root<Company> root = cr.from(Company.class);
+            cr.select(root).where(cb.equal(root.get("name"), name));
+
+            Query<Company> query = session.createQuery(cr);
+            List<Company> companies = query.getResultList();
+            return companies;
+        }
+    }
+
+    public static List<Company> companiesWithNameNotEqualTo(String name) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Company> cr = cb.createQuery(Company.class);
+            Root<Company> root = cr.from(Company.class);
+            cr.select(root).where(cb.notEqual(root.get("name"), name));
+
+            Query<Company> query = session.createQuery(cr);
+            List<Company> companies = query.getResultList();
+            return companies;
+        }
+    }
+
+    public static List<Company> companiesWithNameLike(String name) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Company> cr = cb.createQuery(Company.class);
+            Root<Company> root = cr.from(Company.class);
+            cr.select(root).where(cb.like(root.get("name"), "%" + name + "%"));
+
+            Query<Company> query = session.createQuery(cr);
+            List<Company> companies = query.getResultList();
+            return companies;
+        }
+    }
+
+    public static List<Company> companiesWithNameNotLike(String name) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Company> cr = cb.createQuery(Company.class);
+            Root<Company> root = cr.from(Company.class);
+            cr.select(root).where(cb.notLike(root.get("name"), "%" + name + "%"));
+
+            Query<Company> query = session.createQuery(cr);
+            List<Company> companies = query.getResultList();
+            return companies;
+        }
+    }
+
+    public static List<Company> companiesWithFoundationDateGreaterThan(LocalDate localDate) {
+        try (Session session = SessionFactoryUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Company> cr = cb.createQuery(Company.class);
+            Root<Company> root = cr.from(Company.class);
+            cr.select(root).where(cb.greaterThan(root.get("foundationDate"), localDate));
+
+            Query<Company> query = session.createQuery(cr);
+            List<Company> companies = query.getResultList();
+            return companies;
         }
     }
 }
